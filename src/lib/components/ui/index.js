ee34317ca7e11bebe7a0cd43829c1a19ec04d831
Oliver Walton <oliverharrison03@gmail.com>
2026-04-19 10:20:12 +0100

    chore: remove unneeded index.js

--- src/lib/components/ui/index.js
+++
@@ -1,9 +0,0 @@
-export { Button } from './button/index.js';
-export { ButtonGroup } from './button-group/index.js';
-export { Calendar } from './calendar/index.js';
-export { Checkbox } from './checkbox/index.js';
-export { Input } from './input/index.js';
-export { Popover, PopoverTrigger, PopoverContent } from './popover/index.js';
-export { Select, SelectContent, SelectItem, SelectTrigger } from './select/index.js';
-export { Slider } from './slider/index.js';
-export { Switch } from './switch/index.js';
